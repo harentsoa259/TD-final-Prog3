be97af1a4a065df3ee0ddb12052df447774b946d
harensoa259 <narovanaharentsoa@gmail.com>
2026-04-17 19:33:08 +0300

    feat: add database schema definition

---
+++ src/main/java/org/example/agroptima/AgroptimaApplication.java
@@ -0,0 +1,13 @@
+package org.example.agroptima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroptimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroptimaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/agroptima/AgroptimaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.agroptima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroptimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
