485f54850a38adf6141575ea61665be0357cfb56
harensoa259 <narovanaharentsoa@gmail.com>
2026-04-21 15:27:31 +0300

    feat: update DataConfig with dedicated user123 and fix username setter

--- src/main/java/org/example/agroptima/Repository/DataConfig.java
+++ src/main/java/org/example/agroptima/Repository/DataConfig.java
@@ -8,18 +8,20 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataConfig {
-    private final String url = "jdbc:postgresql://localhost:5432/agroptima";
-    private final String user = "postgres";
-    private final String password = "harentsoa";
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(url);
-        dataSource.setUsername(user);
-        dataSource.setPassword(password);
+
+        dataSource.setUrl(url != null ? url : "jdbc:postgresql://localhost:5432/agroptima");
+        dataSource.setUsername(user != null ? user : "user123");
+        dataSource.setPassword(password != null ? password : "123456");
 
         return dataSource;
     }
