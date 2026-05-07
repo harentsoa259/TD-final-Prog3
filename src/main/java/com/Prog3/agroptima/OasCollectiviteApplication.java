cb3be528a59a3c0a9aaef7e0946c9249c899af38
Ange-H0610 <hei.iraka.4@gmail.com>
2026-05-07 10:46:08 +0300

    chore: setup Spring Boot application with configuration, documentation and test scaffolding

---
+++ src/main/java/com/Prog3/agroptima/OasCollectiviteApplication.java
@@ -0,0 +1,13 @@
+package com.Prog3.agroptima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasCollectiviteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OasCollectiviteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/Prog3/agroptima/AgroptimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prog3.agroptima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroptimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
